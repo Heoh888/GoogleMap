327e4417c9557ecd1c1fa6275fd937a6e66022c2
Aleksei KHodakov <Heoh888@ya.ru>
2022-12-15 20:33:59 +0300

    Initial Commit

---
+++ GoogleMap/GoogleMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleMapApp.swift
+//  GoogleMap
+//
+//  Created by Алексей Ходаков on 15.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct GoogleMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
