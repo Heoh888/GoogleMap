5c4063490be53084f8269a5d71cbd159641515ef
Aleksei KHodakov <Heoh888@ya.ru>
2022-12-15 21:14:28 +0300

    Create an architecture

---
+++ GoogleMap/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  GoogleMap
+//
+//  Created by Алексей Ходаков on 15.12.2022.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print(">> your code here !!")
+        return true
+    }
+}
---
+++ GoogleMap/ViewModel/MapViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewModel.swift
+//  GoogleMap
+//
+//  Created by Алексей Ходаков on 15.12.2022.
+//
+
+import Foundation
