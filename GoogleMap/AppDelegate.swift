0d89f2eb83322c3236fc8428bb98e6f8a9d3836d
Aleksei KHodakov <Heoh888@ya.ru>
2022-12-15 21:19:43 +0300

    Implement Google maps

--- GoogleMap/AppDelegate.swift
+++ GoogleMap/AppDelegate.swift
@@ -6,10 +6,11 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        print(">> your code here !!")
+        GMSServices.provideAPIKey("AIzaSyAsk1leCjxQOxWmA9HL_SEb0LzWYQdS4aI")
         return true
     }
 }
--- GoogleMap/View/MapView.swift
+++ GoogleMap/View/MapView.swift
@@ -6,15 +6,19 @@
 //
 
 import SwiftUI
+import GoogleMaps
 
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct MapView: UIViewRepresentable {
+    
+    private let zoom: Float = 15.0
+    
+    func makeUIView(context: Self.Context) -> GMSMapView {
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        return mapView
     }
-}
-
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView()
+    
+    func updateUIView(_ mapView: GMSMapView, context: Context) {
+        
     }
 }
