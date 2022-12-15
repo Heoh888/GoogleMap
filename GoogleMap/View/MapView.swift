c389bc796277370c3c7e3e00b39a489576d4e24e
Aleksei KHodakov <Heoh888@ya.ru>
2022-12-16 00:41:55 +0300

    Enable geolocation

--- GoogleMap/View/MapView.swift
+++ GoogleMap/View/MapView.swift
@@ -1,5 +1,5 @@
 //
-//  MapView.swift
+//  MapsView.swift
 //  GoogleMap
 //
 //  Created by Алексей Ходаков on 15.12.2022.
@@ -10,15 +10,20 @@ import GoogleMaps
 
 struct MapView: UIViewRepresentable {
     
+    @ObservedObject var locationManager = MapViewModel()
     private let zoom: Float = 15.0
     
     func makeUIView(context: Self.Context) -> GMSMapView {
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: locationManager.latitude, longitude: locationManager.longitude, zoom: zoom)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         return mapView
     }
     
     func updateUIView(_ mapView: GMSMapView, context: Context) {
-        
+        mapView.animate(toLocation: CLLocationCoordinate2D(latitude: locationManager.latitude, longitude: locationManager.longitude))
+        if let coordinate = locationManager.location?.coordinate {
+            let marker = GMSMarker(position: coordinate)
+            marker.map = mapView
+        }
     }
 }
--- GoogleMap/ViewModel/MapViewModel.swift
+++ GoogleMap/ViewModel/MapViewModel.swift
@@ -5,4 +5,42 @@
 //  Created by Алексей Ходаков on 15.12.2022.
 //
 
-import Foundation
+import Combine
+import CoreLocation
+
+class MapViewModel: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+
+    // 1
+     @Published var location: CLLocation? {
+       willSet { objectWillChange.send() }
+     }
+    
+    // 2
+    var latitude: CLLocationDegrees {
+        return location?.coordinate.latitude ?? 0
+    }
+    
+    var longitude: CLLocationDegrees {
+        return location?.coordinate.longitude ?? 0
+    }
+    
+    // 3
+    override init() {
+      super.init()
+
+      locationManager.delegate = self
+      locationManager.desiredAccuracy = kCLLocationAccuracyBest
+      locationManager.requestWhenInUseAuthorization()
+      locationManager.startUpdatingLocation()
+    }
+}
+
+extension MapViewModel: CLLocationManagerDelegate {
+  // 4
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        print(location.coordinate)
+        self.location = location
+    }
+}
